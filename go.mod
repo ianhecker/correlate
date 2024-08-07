9c9e80141b46ed7a10b65d3b946aba95d329a014
ian hecker <ian@blocky.rocks>
2024-08-07 14:26:21 -0600

    Feature[correlate] Add package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ianhecker/correlate
 
 go 1.21.5
+
+require golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
---
+++ internal/correlate/correlate.go
@@ -0,0 +1,49 @@
+package correlate
+
+import (
+	"math"
+	"sort"
+)
+
+func Mean(numbers ...float64) float64 {
+	if len(numbers) == 0 {
+		return 0
+	}
+	var sum float64
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum / float64(len(numbers))
+
+}
+
+func Median(numbers ...float64) float64 {
+	numbers2 := make([]float64, len(numbers))
+	copy(numbers2, numbers)
+
+	sort.Float64s(numbers2)
+
+	var median float64
+	length := len(numbers2)
+	if length == 0 {
+		return 0
+	} else if length%2 == 0 {
+		median = (numbers2[length/2-1] + numbers2[length/2]) / 2
+	} else {
+		median = numbers2[length/2]
+	}
+
+	return median
+}
+
+func StandardDeviation(numbers ...float64) float64 {
+	mean := Mean(numbers...)
+
+	var sum float64
+	for _, num := range numbers {
+		sum += math.Pow(num-mean, 2)
+	}
+
+	variance := sum / float64(len(numbers))
+	return math.Sqrt(variance)
+}
