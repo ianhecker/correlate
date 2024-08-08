c44f833793e7e11014e29eb73bf6d679ae4230fe
ian hecker <ian@blocky.rocks>
2024-08-08 11:04:57 -0600

    Chore[statistics] Add package

---
+++ internal/oracle/statistics.go
@@ -0,0 +1,56 @@
+package oracle
+
+import (
+	"fmt"
+
+	"github.com/ianhecker/correlate/internal/correlate"
+	"github.com/ianhecker/correlate/internal/matrix"
+)
+
+type BasicStatistics struct {
+	ID                string
+	Mean              float64
+	Median            float64
+	Max               float64
+	Min               float64
+	StandardDeviation float64
+}
+
+func MakeBasicStatistics(
+	id string,
+	numbers ...float64,
+) BasicStatistics {
+	return BasicStatistics{
+		ID:                id,
+		Mean:              correlate.Mean(numbers...),
+		Median:            correlate.Median(numbers...),
+		Max:               correlate.Max(numbers...),
+		Min:               correlate.Min(numbers...),
+		StandardDeviation: correlate.StandardDeviation(numbers...),
+	}
+}
+
+func (b BasicStatistics) Strings() []string {
+	return []string{
+		b.ID,
+		fmt.Sprintf("%f", b.Mean),
+		fmt.Sprintf("%f", b.Median),
+		fmt.Sprintf("%f", b.Max),
+		fmt.Sprintf("%f", b.Min),
+		fmt.Sprintf("%f", b.StandardDeviation),
+	}
+}
+
+type Statistics [][]string
+
+func MakeStatistics(
+	headers []string,
+	data ...[]string,
+) Statistics {
+	var stats = [][]string{}
+
+	stats = append(stats, headers)
+	stats = append(stats, data...)
+
+	return matrix.TransposeString(stats)
+}
